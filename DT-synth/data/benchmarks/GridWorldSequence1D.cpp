566f8abc44b3036ba1e8521dce9aab8d6e192f87
Oliver Markgraf <markgraf-oli@web.de>
2018-11-21 10:42:19 +0100

    changed destination path of scalable c++ programs

--- DT-synth/data/benchmarks/GridWorldSequence1D.cpp
+++ DT-synth/data/benchmarks/GridWorldSequence1D.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char* argv[])
 {
 	std::ofstream myfile;
-	myfile.open("data/examples/GridWorldSequence1D.json");
+	myfile.open("data/benchmarks/GridWorldSequence1D.json");
 	int i = std::atoi(argv[1]);
 	int j = i/2;
 	myfile << "{\"variables\":[\"x\",\"y\"],\"variables_dash\":[\"x_dash\",\"y_dash\"],\"successors\":10,\"exprs\":[],\"smt2\":\"(assert (and(<= " << 0 <<" x)(and (<= x " << j <<") (= y 0))))(assert (and(<= x " << i-1 <<")(>= x 0)))(assert (= y 0))(assert (= y 1))(assert(or(or(and(<= x " << j << ")(and(= y 0)(and(= y (- 1 y_dash))(or(= x (+ x_dash 1))(= x x_dash)))))(and(<= " << j << " x)(and(= y 0)(and(= y (- 1 y_dash))(= x x_dash)))))(and(= y 1)(and(= y_dash (- 1 y))(or(= x x_dash)(= x (- x_dash 1)))))))\"}";
--- DT-synth/data/benchmarks/square.cpp
+++ DT-synth/data/benchmarks/square.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char* argv[])
 {
 	std::ofstream myfile;
-	myfile.open("data/examples/square.json");
+	myfile.open("data/benchmarks/square.json");
 	int i = std::atoi(argv[1]);
 	myfile << "{\"variables\":[\"x\",\"y\",\"z\"],\"variables_dash\":[\"x_dash\",\"y_dash\",\"z_dash\"],\"successors\":10,\"exprs\":[],\"smt2\":\"(assert(and (and(= x 0) (= y 0)) (= z 0)))(assert(and(and(and(<= x " << argv[1] << ")(<= -"<< argv[1]<<" x))(<= y "<<argv[1]<<"))(<= -"<< argv[1]<<" y)))(assert(= z 0))(assert(= z 1))(assert(and(or(or(= x x_dash)(= x (+ x_dash 1)))(= x (- x_dash 1)))(and(or(or(= y y_dash)(= y (+ y_dash 1)))(= y (- y_dash 1)))(= z (- 1 z_dash)))))\"}";
 }
